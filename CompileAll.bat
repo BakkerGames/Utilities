6ad328299c9a2dcb46837e2da957c6c7515efdc8
Scott Bakker <sbakker@customdisability.com>
2017-06-12 09:20:44 -0400

    Set all ..\Bin files to readwrite, just in case

--- CompileAll.bat
+++ CompileAll.bat
@@ -9,6 +9,10 @@ if not exist %UPDVERS% set UPDVERS="\Utilities\Bin\UpdateVersions2.exe"
 
 %UPDVERS% "."
 
+attrib -r /s Bin\*.* >nul 2>nul
+
 call BuildAll.bat
 
 del BuildAll.bat>nul 2>nul
+
+attrib +r /s Bin\*.* >nul 2>nul
