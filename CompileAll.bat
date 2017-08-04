29bbd4f752c33211226b60ba36244d28dd4d1fa1
Scott Bakker <sbakker@customdisability.com>
2017-08-04 11:37:03 -0400

    Check for error level after compiiling to prevent deleting the batch file

--- CompileAll.bat
+++ CompileAll.bat
@@ -11,6 +11,7 @@ if not exist %UPDVERS% set UPDVERS="\Utilities\Bin\UpdateVersions2.exe"
 if errorlevel 1 goto error
 
 call BuildAll.bat
+if errorlevel 1 goto error
 
 del BuildAll.bat>nul 2>nul
 goto done
