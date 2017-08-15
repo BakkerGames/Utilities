4666360cb5a1637b7ca7118bb33b3e157a532e08
Scott Bakker <sbakker@customdisability.com>
2017-08-15 14:58:13 -0400

    Strip out multiline comments, junk SET commands, etc

--- FixCreateTableScripts/FixCreateTableScripts/Program.cs
+++ FixCreateTableScripts/FixCreateTableScripts/Program.cs
@@ -1,4 +1,4 @@
-﻿// Program.cs - 08/04/2017
+﻿// Program.cs - 08/15/2017
 
 using System;
 using System.IO;
@@ -8,12 +8,15 @@ namespace FixCreateTableScripts
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            if (args == null || args.Length == 0)
+            if (args == null || args.Length == 0 || !Directory.Exists(args[0]))
             {
                 Console.WriteLine("Syntax: FixCreateTableScripts <path>");
-                return;
+#if DEBUG
+                Console.ReadKey();
+#endif
+                return (1);
             }
             StringBuilder sb = new StringBuilder();
             StringBuilder def = new StringBuilder();
@@ -26,6 +29,7 @@ namespace FixCreateTableScripts
             bool pastTable;
             bool pastAlter;
             bool skipNextGo;
+            bool inMultiLineComment;
             int posStart;
             int posEnd;
             foreach (string filename in Directory.GetFiles(args[0], "*.Table.sql"))
@@ -38,6 +42,7 @@ namespace FixCreateTableScripts
                 pastTable = false;
                 pastAlter = false;
                 skipNextGo = false;
+                inMultiLineComment = false;
                 string defLine;
                 string outLine;
                 tableName = "";
@@ -45,6 +50,26 @@ namespace FixCreateTableScripts
                 {
                     outLine = line.TrimEnd();
                     lineUC = line.TrimEnd().ToUpper();
+                    string outLine2 = outLine;
+                    // remove multiline comments
+                    if (!inMultiLineComment && lineUC.TrimStart().StartsWith("/*"))
+                    {
+                        inMultiLineComment = true;
+                    }
+                    if (inMultiLineComment)
+                    {
+                        if (lineUC.EndsWith("*/"))
+                        {
+                            inMultiLineComment = false;
+                            hasChanges = true;
+                            continue;
+                        }
+                        else
+                        {
+                            hasChanges = true;
+                            continue;
+                        }
+                    }
                     // fix standard issues with create table scripts
                     if (string.IsNullOrEmpty(lineUC))
                     {
@@ -56,13 +81,6 @@ namespace FixCreateTableScripts
                         skipNextGo = false;
                         hasChanges = true;
                         continue;
-                    }
-                    posStart = lineUC.IndexOf("SET QUOTED_IDENTIFIER OFF");
-                    if (lineUC == "SET QUOTED_IDENTIFIER OFF")
-                    {
-                        lineUC = "SET QUOTED_IDENTIFIER ON";
-                        outLine = lineUC;
-                        hasChanges = true;
                     }
                     if (lineUC.Contains(", FILLFACTOR = 90") || lineUC.Contains(", FILLFACTOR = 95"))
                     {
@@ -170,6 +188,42 @@ namespace FixCreateTableScripts
                             hasChanges = true;
                         }
                     }
+                    // remove junk SET statements
+                    if (lineUC.StartsWith("SET QUOTED_IDENTIFIER") ||
+                        lineUC.StartsWith("SET ANSI_NULLS") ||
+                        lineUC.StartsWith("SET ANSI_PADDING"))
+                    {
+                        skipNextGo = true;
+                        hasChanges = true;
+                        continue;
+                    }
+                    // tab expansion and replacement
+                    while (outLine.Contains("\t"))
+                    {
+                        posStart = outLine.IndexOf('\t');
+                        outLine = $"{outLine.Substring(0, posStart)}{new string(' ', 4 - (posStart % 4))}{outLine.Substring(posStart + 1)}";
+                    }
+                    if (outLine.StartsWith("    "))
+                    {
+                        int firstChar = 0;
+                        for (int i = 0; i < outLine.Length; i++)
+                        {
+                            if (outLine[i] != ' ')
+                            {
+                                firstChar = i;
+                                break;
+                            }
+                        }
+                        if (firstChar > 0 && firstChar == (firstChar / 4) * 4)
+                        {
+                            outLine = $"{new string('\t', firstChar / 4)}{outLine.Substring(firstChar)}";
+                        }
+                    }
+                    if (!outLine2.Equals(outLine))
+                    {
+                        hasChanges = true;
+                    }
+                    // done with this line
                     sb.AppendLine(outLine);
                 }
                 if (hasChanges)
@@ -187,6 +241,7 @@ namespace FixCreateTableScripts
 #if DEBUG
             Console.ReadKey();
 #endif
+            return (0);
         }
     }
 }
