33a87bf425f9eb08374f5ca739cc5fd18e21f3d6
Scott Bakker <sbakker@customdisability.com>
2017-08-23 10:13:01 -0400

    Ignore ClearUserPrograms/Settings.bat, as they have hardcoded paths

--- ClearUserSettings.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Are you sure you want to delete all the User Settings?
-echo.
-pause
-SET TARGETPATH="%USERPROFILE%\Applications\Utilities_PC"
-attrib -r %TARGETPATH%\*.settings >nul 2>nul
-del %TARGETPATH%\*.settings >nul 2>nul
-echo --- Done ---
-pause
