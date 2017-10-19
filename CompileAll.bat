a12b022b736956182350767fec8c974c0d0cd77b
Scott <sbakker@customdisability.com>
2017-10-19 17:47:33 -0400

    Changed paths

--- CompileAll.bat
+++ CompileAll.bat
@@ -3,7 +3,7 @@
 attrib -r Build*.bat >nul 2>nul
 del Build*.bat       >nul 2>nul
 
-set UPDVERS="P:\Utilities\Bin\UpdateVersions2.exe"
+set UPDVERS="P:\Bin\Utilities\UpdateVersions2.exe"
 if not exist %UPDVERS% set UPDVERS="\Bin\Utilities\UpdateVersions2.exe"
 
 %UPDVERS% "."
