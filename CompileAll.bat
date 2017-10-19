7ffb5e5d1821424d53ac748c2ca41559ac6ef16a
sbakker <sbakker@customdisability.com>
2017-10-19 13:11:51 -0400

    Changed target object path to \Bin\Utilities

--- CompileAll.bat
+++ CompileAll.bat
@@ -4,8 +4,7 @@ attrib -r Build*.bat >nul 2>nul
 del Build*.bat       >nul 2>nul
 
 set UPDVERS="P:\Utilities\Bin\UpdateVersions2.exe"
-if not exist %UPDVERS% set UPDVERS="C:\Utilities\Bin\UpdateVersions2.exe"
-if not exist %UPDVERS% set UPDVERS="\Utilities\Bin\UpdateVersions2.exe"
+if not exist %UPDVERS% set UPDVERS="\Bin\Utilities\UpdateVersions2.exe"
 
 %UPDVERS% "."
 @if errorlevel 1 goto error
