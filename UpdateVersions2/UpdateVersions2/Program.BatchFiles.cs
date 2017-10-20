4eb8d7c06ce3b744551b03d795a900820d28b5a1
Scott <sbakker@customdisability.com>
2017-10-20 10:24:32 -0400

    Don't delete EXE and DLL files

--- UpdateVersions2/UpdateVersions2/Program.BatchFiles.cs
+++ UpdateVersions2/UpdateVersions2/Program.BatchFiles.cs
@@ -1,5 +1,7 @@
-﻿// Programs.BatchFiles.cs - 08/04/2017
+﻿// Programs.BatchFiles.cs - 10/20/2017
 
+// 10/20/2017 - SBakker
+//            - Don't delete EXE and DLL files for now. They are no longer stored in a relative location.
 // 08/04/2017 - SBakker
 //            - Set errorlevel to 1 when getting any errors.
 // 05/22/2017 - SBakker
@@ -68,23 +70,23 @@ namespace UpdateVersions2
             result.AppendLine("for /f \"delims=;\" %%a in (_delbin.txt) do rmdir /s /q \"%%a\"");
             result.AppendLine("del _delbin.txt >nul 2>nul");
             result.AppendLine();
-            if (SourceFlag)
-            {
-                result.AppendLine("del ..\\Bin\\*.exe >nul 2>nul");
-                result.AppendLine("del ..\\Bin\\*.dll >nul 2>nul");
-                result.AppendLine("del ..\\Bin\\*.config >nul 2>nul");
-                result.AppendLine("del ..\\Bin\\*.settings >nul 2>nul");
-            }
-            else
-            {
-                result.AppendLine("rename \"Bin\\Arena.xml\" \"Arena_xml.delbin\" >nul 2>nul");
-                result.AppendLine("del Bin\\*.exe >nul 2>nul");
-                result.AppendLine("del Bin\\*.dll >nul 2>nul");
-                result.AppendLine("del Bin\\*.xml >nul 2>nul");
-                result.AppendLine("del Bin\\*.config >nul 2>nul");
-                result.AppendLine("del Bin\\*.settings >nul 2>nul");
-                result.AppendLine("rename \"Bin\\Arena_xml.delbin\" \"Arena.xml\" >nul 2>nul");
-            }
+            //if (SourceFlag)
+            //{
+            //    result.AppendLine("del ..\\Bin\\*.exe >nul 2>nul");
+            //    result.AppendLine("del ..\\Bin\\*.dll >nul 2>nul");
+            //    result.AppendLine("del ..\\Bin\\*.config >nul 2>nul");
+            //    result.AppendLine("del ..\\Bin\\*.settings >nul 2>nul");
+            //}
+            //else
+            //{
+            //    result.AppendLine("rename \"Bin\\Arena.xml\" \"Arena_xml.delbin\" >nul 2>nul");
+            //    result.AppendLine("del Bin\\*.exe >nul 2>nul");
+            //    result.AppendLine("del Bin\\*.dll >nul 2>nul");
+            //    result.AppendLine("del Bin\\*.xml >nul 2>nul");
+            //    result.AppendLine("del Bin\\*.config >nul 2>nul");
+            //    result.AppendLine("del Bin\\*.settings >nul 2>nul");
+            //    result.AppendLine("rename \"Bin\\Arena_xml.delbin\" \"Arena.xml\" >nul 2>nul");
+            //}
             result.AppendLine();
             result.AppendLine("set logfile=\"BuildAll.log\"");
             result.AppendLine("attrib -r %logfile% >nul 2>nul");
