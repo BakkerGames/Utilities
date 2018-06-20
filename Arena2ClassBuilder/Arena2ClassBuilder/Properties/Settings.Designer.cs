58c7b3b445b207be14ca3f0246369d87120392ce
Scott <sbakker@customdisability.com>
2018-06-20 13:16:07 -0400

    added arena_notediary and arena_iap

--- Arena2ClassBuilder/Arena2ClassBuilder/Properties/Settings.Designer.cs
+++ Arena2ClassBuilder/Arena2ClassBuilder/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Arena2ClassBuilder.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -67,7 +67,8 @@ namespace Arena2ClassBuilder.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Advantage;Arena;IDR;IDRIS;IDRIS Advantage;NoteDiary;Security;TempData")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Advantage;Arena;Arena_NoteDiary;Arena_IAP;IDR;IDRIS;IDRIS Advantage;NoteDiary;Sec" +
+            "urity;TempData")]
         public string Apps {
             get {
                 return ((string)(this["Apps"]));
