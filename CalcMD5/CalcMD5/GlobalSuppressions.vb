3e34408c8a111724b0f2326caba01fbf674f1021
ScottB <sbakker@maineturnpike.com>
2020-01-21 11:59:20 -0500

    Working on old CalcMD5

---
+++ CalcMD5/CalcMD5/GlobalSuppressions.vb
@@ -0,0 +1,7 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification:="<Pending>")>
+<Assembly: CodeAnalysis.SuppressMessage("Performance", "CA1820:Test for empty strings using string length", Justification:="<Pending>")>
